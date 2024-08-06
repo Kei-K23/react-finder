497b7888dd3e7dbdaf5a1445f3e1d8f1b638c565
Kei-K23 <keiksl2301@gmail.com>
2024-08-06 15:10:23 +0630

    Add sentry for error management

--- components/filesystem-manage-modal.tsx
+++ components/filesystem-manage-modal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Dialog,
@@ -9,10 +11,46 @@ import {
   FilesystemCreateType,
   useFilesystemManageModalStore,
 } from "@/store/use-filesystem-manage-modal-store";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useFilesystemStore } from "@/store/use-filesystem-store";
+
+const formSchema = z.object({
+  name: z.string().min(1).max(50),
+});
 
 export default function FilesystemManageModal() {
+  const { addNewNode, currentSelectedNode } = useFilesystemStore();
   const { isOpen, onClose, node, type } = useFilesystemManageModalStore();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: node?.name || "",
+    },
+  });
 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!currentSelectedNode) {
+      return;
+    }
+    const newNode =
+      type === FilesystemCreateType.FILE
+        ? { name: values.name }
+        : { name: values.name, nodes: [] };
+    addNewNode(currentSelectedNode?.name!, newNode);
+  }
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="bg-neutral-700 bg-clip-padding backdrop-filter flex items-center flex-col backdrop-blur-xl bg-opacity-100 select-none text-neutral-100 border-gray-500">
@@ -27,6 +65,31 @@ export default function FilesystemManageModal() {
               : "Rename the folder"}
           </DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 w-full"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Awesome"
+                      {...field}
+                      className="text-neutral-900"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Add</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
